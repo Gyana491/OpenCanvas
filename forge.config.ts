3feb4064e52b59eee20234df2585cb7baa7f4ddb
Gyana Ranjan <gyanaranjanmohanta491@gmail.com>
2026-02-05 06:29:31 +0530

    feat: Add DMG maker for macOS builds.

--- forge.config.ts
+++ forge.config.ts
@@ -4,6 +4,7 @@ import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
@@ -25,6 +26,7 @@ const config: ForgeConfig = {
     new MakerZIP({}, ["darwin", "win32"]),
     new MakerRpm({}),
     new MakerDeb({}),
+    new MakerDMG({}),
   ],
   publishers: [
     {
